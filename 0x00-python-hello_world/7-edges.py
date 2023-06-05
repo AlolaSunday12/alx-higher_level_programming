cdb9adf30879bc033f9b2e3c360e32e5baa51eec
AlolaSunday12 <alolasj@gmail.com>
2023-06-05 12:06:52 -0700

    copy cut paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:6]
-word_last_2 = word[7:]
-middle_word = word[4:-4]
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print(f"word_first_3: {word_first_3}")
 print(f"word_last_2: {word_last_2}")
 print(f"middle_word: {middle_word}")
