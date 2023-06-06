91e2ef036f2885deaf8d358ac159ed9f5f26f77c
AlolaSunday12 <alolasj@gmail.com>
2023-06-06 16:38:50 -0700

    smile in the error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i), end="")
-    if i != 97:
-        print("{:c}".format(i - 32), end="")
+for i in range(90, 64, -1):
+    print("{:c}{:c}".format(i + 32, i), end="")
 
 print()
