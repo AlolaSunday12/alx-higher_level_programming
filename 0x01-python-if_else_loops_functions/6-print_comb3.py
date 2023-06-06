41b773222ac613f21e0aad50a844691babc78dff
AlolaSunday12 <alolasj@gmail.com>
2023-06-06 15:36:53 -0700

    two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{0:02d}, {1:02d}".format(tens_digit, ones_digit))
+        print("{:02d}, {:02d}".format(tens_digit, ones_digit))
