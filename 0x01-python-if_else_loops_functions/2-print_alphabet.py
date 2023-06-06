e5254744f83c2bf732258fe07e4e845a13be499b
AlolaSunday12 <alolasj@gmail.com>
2023-06-06 15:19:12 -0700

    print ASCII code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+alphabet = ""
 for ascii_value in range(ord('a'), ord('z')+1):
-    print(chr(ascii_value), end='')
+    alphabet += chr(ascii_value)
+
+print("{}".format(alphabet), end="")
