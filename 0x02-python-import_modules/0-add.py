e26874d86f07a4f08077fc21f607e3a62daa2393
AlolaSunday12 <alolasj@gmail.com>
2023-06-08 05:12:02 -0700

    import a function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+_value_ = "value"
+
+if _value_ == "value":
+    a = 1
+    b = 2
+    from add_0 import add
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
