1e96b5113e64b34eb59cdb9de9b2da6277f78d52
AlolaSunday12 <alolasj@gmail.com>
2023-06-10 15:06:21 -0700

    secure element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@
 # 1-element_at.py
 
 def element_at(my_list, idx):
-    def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+     if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
