675ec3b948fbd809843b46d18df481b0434104ed
AlolaSunday12 <alolasj@gmail.com>
2023-07-05 16:40:30 +0000

    python driven tests

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
 # 101-lazy_matrix_mul.py
-"""Defines a function for matrix multiplication using NumPy."""
-
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Perform matrix multiplication using NumPy."""
-    if not isinstance(m_a, list) or not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b, list) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
-        raise TypeError("m_b should contain only integers or floats")
-    if any(len(row) != len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must have the same size")
-    if any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must have the same size")
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+    """Return the multiplication of two matrices.
 
-    return np.matmul(np.array(m_a), np.array(m_b))
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
