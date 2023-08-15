278141a9503aadefae7d1e9a26c478058e458377
AlolaSunday12 <alolasj@gmail.com>
2023-08-15 12:10:12 -0700

    mysql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
--- Script to update the score of Bob to 10 in the table second_table
-
--- Update the score of Bob to 10
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+-- removes all records where score is <= 5 from
+-- second table
+DELETE FROM second_table WHERE score <= 5;
