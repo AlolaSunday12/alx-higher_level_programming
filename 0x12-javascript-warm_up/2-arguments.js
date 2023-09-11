ec75941aa54255f7d0fe9f4be60f98736a328b88
AlolaSunday12 <alolasj@gmail.com>
2023-09-11 14:49:15 +0000

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 const argc = process.argv.length;
 
 if (argc === 2) {
-console.log('No argument');
-} else if (argc === 3) {
 console.log('Argument found');
-} else {
+} else if (argc > 2) {
 console.log('Arguments found');
+} else {
+console.log('NO argument');
 }
