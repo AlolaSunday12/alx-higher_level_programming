483dbe850d677377f9f88bd3f7a18e8a8cf1ae92
AlolaSunday12 <alolasj@gmail.com>
2023-09-19 12:43:25 +0000

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3 
- """ 
- This	script�lists all states from the 
- database `hbtn_0e_0_usa`. 
- """ 
-  
- import MySQLdb 
- from sys import argv 
-  
- if __name__ == '__main__': 
-     """ 
-     Access to the database and get the states 
-     from the database. 
-     """ 
-     db_connect = MySQLdb.connect( 
-         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]) 
-  
-     db_cursor = db_connect.cursor() 
-  
-     db_cursor.execute("SELECT * FROM states") 
-  
-     rows_selected = db_cursor.fetchall() 
-  
-     for row in rows_selected: 
-         print(row)
+""" 
+This	script�lists all states from the 
+database `hbtn_0e_0_usa`. 
+""" 
+
+import MySQLdb 
+from sys import argv 
+
+if __name__ == '__main__': 
+    """ 
+    Access to the database and get the states 
+    from the database. 
+    """ 
+    db_connect = MySQLdb.connect( 
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]) 
+
+    db_cursor = db_connect.cursor() 
+
+    db_cursor.execute("SELECT * FROM states") 
+
+    rows_selected = db_cursor.fetchall() 
+
+    for row in rows_selected: 
+        print(row)
