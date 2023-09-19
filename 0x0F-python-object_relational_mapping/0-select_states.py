ed1dee6ba77379995d6978725e1b19c3cedf15c2
AlolaSunday12 <alolasj@gmail.com>
2023-09-19 13:49:16 +0000

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,16 @@
-#!/usr/bin/python3 
-""" 
-This	script�lists all states from the 
-database `hbtn_0e_0_usa`. 
-""" 
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-import MySQLdb 
-from sys import argv 
 
-if __name__ == '__main__': 
-    """ 
-    Access to the database and get the states 
-    from the database. 
-    """ 
-    db_connect = MySQLdb.connect( 
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]) 
-
-    db_cursor = db_connect.cursor() 
-
-    db_cursor.execute("SELECT * FROM states") 
-
-    rows_selected = db_cursor.fetchall() 
-
-    for row in rows_selected: 
-        print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,1 +1,1 @@
-0x0F-python-object_relational_mapoing
+0x0F-python-object_relational_mapping.
